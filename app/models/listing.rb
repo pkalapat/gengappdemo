c92e4194b513aa9f17c5ef7dab7b2ede23fb0713
Pallavi Kalapatapu <pkalapat@gmail.com>
2015-01-22 00:48:20 -0800

    restored images. ran figaro heroku:set -e production to settle dropbox access from Heroku. things seem happy now

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,13 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
-  		has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   		
-    else                
+  if Rails.env.production?                
      	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
                     :storage => :dropbox,
                     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                     :path => ":style/:id_:filename"
+  else
+
+      has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
                     
    end
      validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
