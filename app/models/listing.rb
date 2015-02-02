5ed58370ca4bbcc22ab20e9a099ea1a441497aab
Pallavi Kalapatapu <pkalapat@gmail.com>
2015-02-01 22:04:51 -0800

    Add validations for users and listings.some how validations always show red bar not green bar (success case not invoked. need to check out again

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,6 +10,9 @@ class Listing < ActiveRecord::Base
       has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
                     
    end
+     validates :name, :description, :price, presence: true
+     validates :price, numericality: { greater_than: 0}
+     validates_attachment_presence :image
      validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
 
